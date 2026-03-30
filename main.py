890d38a4fd79ac56ca9d08e6cf3ddcfc178b6091
codeforge227-dotcom <codeforge227@gmail.com>
2026-03-30 21:33:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import os
 import discord
 from discord.ext import commands
 from discord import FFmpegPCMAudio
-import yt_dlp as youtube_dl  # nova zamena za youtube_dl
 import asyncio
 
+# Tvoj bot token iz Railway env var
 TOKEN = os.getenv("TOKEN")
+
+# ID voice kanala gde bot ulazi
 VOICE_CHANNEL_ID = 1193137115387678761
+
+# Direktan TOP FM stream (128 kbps)
+RADIO_URL = "https://topfm128ssl.streaming.rs:9282/;stream.mp3"
 
 intents = discord.Intents.default()
 client = commands.Bot(command_prefix="!", intents=intents)
-
-# Tvoj YouTube live radio link
-MUSIC_URL = "https://www.youtube.com/live/8V5EWDtA474?si=saGuH_01iOB7juEw"
 
 @client.event
 async def on_ready():
@@ -25,32 +27,18 @@ async def on_ready():
 
     if not channel.guild.voice_client:
         vc = await channel.connect()
-        print("Bot povezan u kanal. Pustam muziku...")
+        print("Povezan u kanal. Pustam TOP FM...")
 
-        # yt-dlp opcije
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'quiet': True,
-            'no_warnings': True,
-            'extractaudio': True,
-            'audioformat': "mp3",
-        }
-
-        # preuzimanje direktnog audio streama
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(MUSIC_URL, download=False)
-            url2 = info['url']
-
-        # pustanje muzike preko FFmpeg
+        # FFmpeg opcije da stream ne stane
         ffmpeg_opts = {
             'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
             'options': '-vn'
         }
 
-        vc.play(FFmpegPCMAudio(url2, **ffmpeg_opts))
+        vc.play(FFmpegPCMAudio(RADIO_URL, **ffmpeg_opts))
         print("Muzika se pušta...")
 
-        # čekanje dok se muzika pušta
+        # Čuvamo petlju dok muzika svira
         while vc.is_playing():
             await asyncio.sleep(1)
 
