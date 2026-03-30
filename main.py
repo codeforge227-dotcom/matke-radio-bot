5c67839d67e7d8ca7dfc310077f218c6c8a4a7e8
codeforge227-dotcom <codeforge227@gmail.com>
2026-03-30 21:15:21 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import os
 import discord
+from discord import FFmpegPCMAudio
+import asyncio
 
+# Tvoj bot token iz Railway env var
 token = os.getenv("TOKEN")
+
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
+
+# ID voice kanala gde će bot ući
+VOICE_CHANNEL_ID = 1193137115387678761
+
+# Direktan stream URL koji radi
+RADIO_URL = "https://stream.playradio.rs:8001/play.mp3"
 
 @client.event
 async def on_ready():
     print(f"Ulogovan kao {client.user}")
-    channel = client.get_channel(1193137115387678761)
+    channel = client.get_channel(VOICE_CHANNEL_ID)
     if channel and not client.voice_clients:
         vc = await channel.connect()
-        # Direktan MP3 stream koji radi
-        url = "https://stream.playradio.rs:8001/play.mp3"
-        vc.play(discord.FFmpegPCMAudio(url))
-        print("Pustam muziku...")
+        print("Povezan u kanal. Pustam muziku...")
+
+        # Pusti radio stream
+        vc.play(FFmpegPCMAudio(RADIO_URL), after=lambda e: print("Stream završen", e))
+
+        # Održava vezu dok se muzika pušta
+        while vc.is_playing():
+            await asyncio.sleep(1)
 
 client.run(token)
