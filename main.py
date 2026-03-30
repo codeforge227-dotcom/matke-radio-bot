7fbe9b42907e4792372dcfe8aa9b9e1de57b985f
codeforge227-dotcom <codeforge227@gmail.com>
2026-03-30 21:19:37 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 import os
 import discord
-from discord import FFmpegPCMAudio
+from discord import FFmpegOpusAudio
 import asyncio
 
-# Tvoj bot token iz Railway env var
-token = os.getenv("TOKEN")
+# Bot token iz Railway env var
+TOKEN = os.getenv("TOKEN")
 
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-
-# ID voice kanala gde će bot ući
+# ID voice kanala gde bot ulazi
 VOICE_CHANNEL_ID = 1193137115387678761
 
-# Direktan stream URL koji radi
-RADIO_URL = "https://stream.playradio.rs:8001/play.mp3"
+# Direktan radio stream koji radi
+RADIO_URL = "https://naxi128.streaming.rs:9152/;*.mp3"  # testiran, radi sa botom
+
+# Kreiramo client
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f"Ulogovan kao {client.user}")
+    
+    # Pronađi kanal
     channel = client.get_channel(VOICE_CHANNEL_ID)
-    if channel and not client.voice_clients:
+    if channel is None:
+        print("Voice kanal nije pronađen!")
+        return
+
+    # Ako bot još nije u kanalu
+    if not client.voice_clients:
         vc = await channel.connect()
-        print("Povezan u kanal. Pustam muziku...")
+        print("Bot povezan u kanal. Pustam muziku...")
 
-        # Pusti radio stream
-        vc.play(FFmpegPCMAudio(RADIO_URL), after=lambda e: print("Stream završen", e))
+        # Opcije za reconnect da stream ne stane
+        before_opts = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+        audio_source = FFmpegOpusAudio(RADIO_URL, before_options=before_opts)
 
-        # Održava vezu dok se muzika pušta
+        vc.play(audio_source, after=lambda e: print("Stream završen", e))
+
+        # Čuvamo petlju da bot ostane povezan i muzika se pušta
         while vc.is_playing():
             await asyncio.sleep(1)
 
-client.run(token)
+# Start bota
+client.run(TOKEN)
